0cd9e5730c784ff08992a82973a62193d38cfc26
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-06-09 16:39:38 +0700

    Setting API

---
+++ src/api/tmdbApi.ts
@@ -0,0 +1,58 @@
+import axiosClient from "./axiosClient";
+
+type CategoryType = {
+  movie: "movie";
+  tv: "tv";
+};
+
+type MovieType = {
+  upcoming: "upcoming";
+  popular: "popular";
+  top_rated: "top_rated";
+};
+
+type TvType = {
+  popular: "popular";
+  top_rated: "top_rated";
+  on_the_air: "on_the_air";
+};
+
+const tmdbApi = {
+  getMoviesList: (type: MovieType, params: object) => {
+    const url: string = "movie/" + type;
+    return axiosClient.get(url, params);
+  },
+
+  getTvList: (type: TvType, params: object) => {
+    const url: string = "tv/" + type;
+    return axiosClient.get(url, params);
+  },
+
+  getVideos: (cate: CategoryType, id: number) => {
+    const url: string = cate + "/" + id + "/videos";
+    return axiosClient.get(url, { params: {} });
+  },
+
+  search: (cate: CategoryType, params: object) => {
+    const url: string = "search/" + cate;
+    return axiosClient.get(url, params);
+  },
+
+  detail: (cate: CategoryType, id: number, params: object) => {
+    const url: string = cate + "/" + id;
+    return axiosClient.get(url, params);
+  },
+
+  credits: (cate: CategoryType, id: number) => {
+    const url: string = cate + "/" + id + "/credits";
+    return axiosClient.get(url, { params: {} });
+  },
+
+  similar: (cate: CategoryType, id: number) => {
+    const url: string = cate + "/" + id + "/similar";
+    return axiosClient.get(url, { params: {} });
+  },
+};
+
+export default tmdbApi;
+ 
---
+++ src/config/Routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import Home from "@/pages/home";
+import Catalog from "@/pages/catalog";
+import Detail from "@/pages/detail";
+
+type Props = {};
+
+const Routes = (props: Props) => {
+  return <div>Routes</div>;
+};
+
+export default Routes;
